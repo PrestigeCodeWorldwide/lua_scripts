354c1d5eb9d5e30dd6e6c8f5701107a54d7fbe02
Satchago <143149592+Satchago@users.noreply.github.com>
2026-01-13 01:59:37 -0500

    Refactor to use script coordination w/ offtank lua

--- Doomshade.lua
+++ Doomshade.lua
@@ -2,7 +2,7 @@ local mq = require('mq')
 local os = require('os')
 local BL = require('biggerlib')
 
-BL.info("Doomshade script v1.02 loaded.")
+BL.info("Doomshade script v1.05 loaded.")
 
 local viralLocs = {
     { -99, -310, -49 },  -- Viral 4 on map
@@ -22,7 +22,9 @@ local DELAY = 24000
 
 local function delayTillSafeSpot()
     print("Navigating to safe spot.")
-    BL.cmd.WaitForNav()
+    while mq.TLO.Navigation.Active() do
+        mq.delay(100)
+    end
     print("You reached the safe spot.")
 end
 
@@ -36,49 +38,53 @@ end
 local function handleViralEvent(somenames)
     if not BL.nameListIncludesMe(somenames) then return end
     
-    BL.cmd.pauseAutomation()
-    BL.cmd.removeZerkerRootDisc()
-    
-    local names = BL.parseAllNames(somenames)
-    local myname = mq.TLO.Me.CleanName()
-    
-    for i, name in ipairs(names) do
-        if name == myname and viralLocs[i] then
-            print(string.format("Running to Viral spot %d: %d, %d, %d", 
-                  i, viralLocs[i][1], viralLocs[i][2], viralLocs[i][3]))
-            mq.cmdf('/nav locxyz %d %d %d', viralLocs[i][1], viralLocs[i][2], viralLocs[i][3])
-            break
+    BL.cmd.coordinateWithScript("offtank", function()
+        BL.cmd.pauseAutomation()
+        BL.cmd.removeZerkerRootDisc()
+        
+        local names = BL.parseAllNames(somenames)
+        local myname = mq.TLO.Me.CleanName()
+        
+        for i, name in ipairs(names) do
+            if name == myname and viralLocs[i] then
+                print(string.format("Running to Viral spot %d: %d, %d, %d", 
+                      i, viralLocs[i][1], viralLocs[i][2], viralLocs[i][3]))
+                mq.cmdf('/nav locxyz %d %d %d', viralLocs[i][1], viralLocs[i][2], viralLocs[i][3])
+                break
+            end
         end
-    end
-    
-    delayTillSafeSpot()
-    waitAtSafeSpotCountdown()
-    BL.cmd.returnToRaidMainAssist()
-    BL.cmd.resumeAutomation()
+        
+        delayTillSafeSpot()
+        waitAtSafeSpotCountdown()
+        BL.cmd.returnToRaidMainAssist()
+        BL.cmd.resumeAutomation()
+    end)
 end
 
 local function handleDarknessEvent(somenames)
     if not BL.nameListIncludesMe(somenames) then return end
     
-    BL.cmd.pauseAutomation()
-    BL.cmd.removeZerkerRootDisc()
-    
-    local names = BL.parseAllNames(somenames)
-    local myname = mq.TLO.Me.CleanName()
-    
-    for i, name in ipairs(names) do
-        if name == myname and darknessLocs[i] then
-            print(string.format("Running to Darkness spot %d: %d, %d, %d", 
-                  i, darknessLocs[i][1], darknessLocs[i][2], darknessLocs[i][3]))
-            mq.cmdf('/nav locxyz %d %d %d', darknessLocs[i][1], darknessLocs[i][2], darknessLocs[i][3])
-            break
+    BL.cmd.coordinateWithScript("offtank", function()
+        BL.cmd.pauseAutomation()
+        BL.cmd.removeZerkerRootDisc()
+        
+        local names = BL.parseAllNames(somenames)
+        local myname = mq.TLO.Me.CleanName()
+        
+        for i, name in ipairs(names) do
+            if name == myname and darknessLocs[i] then
+                print(string.format("Running to Darkness spot %d: %d, %d, %d", 
+                      i, darknessLocs[i][1], darknessLocs[i][2], darknessLocs[i][3]))
+                mq.cmdf('/nav locxyz %d %d %d', darknessLocs[i][1], darknessLocs[i][2], darknessLocs[i][3])
+                break
+            end
         end
-    end
-    
-    delayTillSafeSpot()
-    waitAtSafeSpotCountdown(15) -- Shorter wait for darkness
-    BL.cmd.returnToRaidMainAssist()
-    BL.cmd.resumeAutomation()
+        
+        delayTillSafeSpot()
+        waitAtSafeSpotCountdown(15) -- Shorter wait for darkness
+        BL.cmd.returnToRaidMainAssist()
+        BL.cmd.resumeAutomation()
+    end)
 end
 
 -- Main event handler
