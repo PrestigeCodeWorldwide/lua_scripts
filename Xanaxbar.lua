e1a18e243d8ccb63940ff2eae1570b427071c6f3
Satchago <143149592+Satchago@users.noreply.github.com>
2026-02-10 16:58:30 -0500

    add turn off alliance on priests

--- Xanaxbar.lua
+++ Xanaxbar.lua
@@ -3,7 +3,7 @@ local mq = require('mq')
 ---@type BL
 local BL = require("biggerlib")
 
-BL.info("Xanaxbar Script 1.0 Started")
+BL.info("Xanaxbar Script 1.01 Started")
 local myClass = mq.TLO.Me.Class.ShortName()
 
 mq.cmdf("/%s byos off nosave", myClass)
@@ -12,3 +12,8 @@ mq.cmdf("/%s usewardaa off nosave", myClass)
 mq.cmdf("/%s usesquall off nosave", myClass)
 mq.cmdf("/%s usesplash off nosave", myClass)
 mq.cmdf("/%s usenatureboon off nosave", myClass)
+
+-- Only disable alliance for priests (DRU, CLR, SHM)
+if myClass == "DRU" or myClass == "CLR" or myClass == "SHM" then
+    mq.cmdf("/%s usealliance off nosave", myClass)
+end
